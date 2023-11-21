65f96c9536f5efb1951b631b43d651a29d7540e1
Stefano <stefe68@gmail.com>
2023-11-21 15:46:10 +0100

    Add GROUP BY queries

--- queries.sql
+++ queries.sql
@@ -70,22 +70,42 @@ AND `release_date` LIKE "2018%"
 
 
 -- *********** GROUP BY ***********
--- 1- Contare quante software house ci sono per ogni paese (3)
+-- 1- Contare quante software house ci sono per ogni paese
+SELECT `country`, COUNT(*) AS `software_houses_number`
+FROM `software_houses`
+GROUP BY `country`;
 
 -- 2- Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)
+SELECT `videogame_id`, COUNT(*) AS `total_reviews`
+FROM `reviews`
+GROUP BY `videogame_id`;
 
 -- 3- Contare quanti videogiochi hanno ciascuna classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)
+SELECT `pegi_label_id`, COUNT(*) AS `PEGI_label`
+FROM `pegi_label_videogame`
+GROUP BY `pegi_label_id`
 
 -- 4- Mostrare il numero di videogiochi rilasciati ogni anno (11)
+SELECT YEAR(`release_date`) AS `anno_rilascio`,
+COUNT(*) AS `numero_videogiochi`
+FROM `videogames`
+GROUP BY YEAR(`release_date`);
 
 -- 5- Contare quanti videogiochi sono disponbiili per ciascun device (del device vogliamo solo l'ID) (7)
+SELECT `device_id`, COUNT(`videogame_id`) AS `videogame_per_device`
+FROM `device_videogame`
+GROUP BY `device_id`
 
 -- 6- Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)
-
+SELECT videogame_id, AVG(rating) AS media_recensioni
+FROM reviews
+GROUP BY videogame_id
+ORDER BY media_recensioni DESC;
 
 
 -- *********** JOIN ***********
 -- 1- Selezionare i dati di tutti giocatori che hanno scritto almeno una recensione, mostrandoli una sola volta (996)
+
 
 -- 2- Sezionare tutti i videogame dei tornei tenuti nel 2016, mostrandoli una sola volta (226)
 
